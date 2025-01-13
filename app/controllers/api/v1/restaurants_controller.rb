82c5e8c3a323407298205cb3c68a2bfe0e0b5ea0
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-13 23:51:39 +0900

    Added route and controller method for show

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def index
     @restaurants = policy_scope(Restaurant)
   end
+
+  def show
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+  end
 end
