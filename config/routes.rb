97ea34b31b42049dbcb0088ae1974d7793cc8acd
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-15 00:24:12 +0900

    New authentication generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
