659adfeff2930cf77d0c9be031c650a00bfa75c4
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-15 21:16:36 +0900

    fixed migration issues

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,25 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_14_151629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "restaurant_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "ip_address"
@@ -31,5 +50,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_14_151629) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "comments", "restaurants"
+  add_foreign_key "comments", "users"
+  add_foreign_key "restaurants", "users"
   add_foreign_key "sessions", "users"
 end
