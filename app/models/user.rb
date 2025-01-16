d82045130e3b4b494c6870b53cb9daaa8d0f3581
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-16 15:56:35 +0900

    added private_api_key to user model on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  encrypts :private_api_key
+  blind_index :private_api_key
+
+  before_create :set_private_api_key
+  validates :private_api_key, uniqueness: true, allow_blank: true
+
+  private
+
+  def set_private_api_key
+    self.private_api_key = SecureRandom.hex if self.private_api_key.nil?
+  end
 end
